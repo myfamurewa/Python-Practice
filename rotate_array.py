003cc7503af6971971f96b85c32ef0dfd5be7d6c
Michael Famurewa <myfamurewa@gmail.com>
2020-10-15 14:21:26 -0400

    added the optimal solution from leetcode

--- rotate_array.py
+++ rotate_array.py
@@ -32,3 +32,20 @@ t1 = [1,2,3,4,5,6,7]
 # print(t1)
 # print(rotateV3(t1, 3))
 print(t1[:])
+
+def rotateOptimal(nums, k):
+    n = len(nums)
+    k %= n
+    
+    start = count = 0
+    while count < n:
+        current, prev = start, nums[start]
+        while True:
+            next_idx = (current + k) % n
+            nums[next_idx], prev = prev, nums[next_idx]
+            current = next_idx
+            count += 1
+            
+            if start == current:
+                break
+        start += 1
